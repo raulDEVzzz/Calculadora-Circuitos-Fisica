f01ef25af07eaff67ff8f717901a67cce9d4c13b
raulDEVzzz <168381591+raulDEVzzz@users.noreply.github.com>
2025-08-05 00:58:08 -0300

    Add files via upload

---
+++ trabalho-fisica-segundo-bimestre/src/main/java/br/edu/unespar/trabalho/fisica/segundo/bimestre/CalculadoraDeCircuito.java
@@ -0,0 +1,18 @@
+package br.edu.unespar.trabalho.fisica.segundo.bimestre;
+
+public class CalculadoraDeCircuito {
+
+    public static ResultadoCalculo calcular(double r1, double r2, double e1, double e2, double e3) throws ArithmeticException {
+        if (r1 == 0 || r2 == 0) {
+            throw new ArithmeticException("Os valores dos resistores não podem ser zero.");
+        }
+
+        double i2 = (e1 - e3) / r2;
+
+        double i1 = (e3 - e2) / r1;
+
+        double v_ab = -r2 * i2;
+
+        return new ResultadoCalculo(i1, i2, v_ab);
+    }
+}
